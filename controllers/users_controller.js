05e7daea53280197b7546c2b54882f6c3ccec06c
786samir <sayyedsamir11@gmail.com>
2021-11-13 20:29:05 +0530

    added sign in and sign up

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,39 +1,38 @@
-const User = require("../models/user");
+const User = require('../models/user');
+
 
 module.exports.profile = function(req, res){
-
-    if(req.cookies.user_id){
-        User.findById(req.cookies.user_id, function(err, user){
-            if(user){
-                return res.render('profile',{
-                    title: "Profile",
-                    user:user
-                });
-            }
-            return res.redirect('/users/sign-in');
-    
-        });
-    }else{
-        return res.redirect('/users/sign-in');
-    } 
+    return res.render('user_profile', {
+        title: 'User Profile'
+    })
 }
-// render the sign in page
+
+
+// render the sign up page
 module.exports.signUp = function(req, res){
-    return res.render('user_sign_up',{
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+
+
+    return res.render('user_sign_up', {
         title: "Codeial | Sign Up"
     })
 }
 
-// render the sign out page
+
+// render the sign in page
 module.exports.signIn = function(req, res){
-    return res.render('user_sign_in',{
+
+    if (req.isAuthenticated()){
+        return res.redirect('/users/profile');
+    }
+    return res.render('user_sign_in', {
         title: "Codeial | Sign In"
     })
 }
 
-
-//get the sign up data
-
+// get the sign up data
 module.exports.create = function(req, res){
     if (req.body.password != req.body.confirm_password){
         return res.redirect('back');
@@ -56,22 +55,13 @@ module.exports.create = function(req, res){
 }
 
 
-//get the sign In data
+// sign in and create a session for the user
 module.exports.createSession = function(req, res){
-  //find the user
-    User.findOne({email: req.body.email}, function(err, user){
-        if(err){console.log('error in finding user in signing'); return}
+    return res.redirect('/');
+}
 
-        if(user){
-            
-            if(user.password != req.body.password){
-                return res.redirect('back');
-            }
-                res.cookie('user_id', user.id);
-                return res.redirect('/users/profile');
-        }else{
-            return res.redirect('back');
-        }
-    })
- 
+module.exports.destroySession = function(req, res){
+    req.logout();
+
+    return res.redirect('/');
 }
